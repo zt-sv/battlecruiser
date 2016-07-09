7e1a6a194ab3fcd193f1fb0d5a164082e9059970
13rentgen <13rentgen@gmail.com>
2016-07-09 15:30:41 +0300

    Base class test

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
   JavaScript framework for building complex [node](http://nodejs.org) applications.
   
+  [![NPM Version](https://img.shields.io/npm/v/battlecruiser.svg)](https://www.npmjs.com/package/battlecruiser)
+  [![NPM Downloads](https://img.shields.io/npm/dm/battlecruiser.svg)](https://www.npmjs.com/package/battlecruiser)
   [![Build Status](https://travis-ci.org/13rentgen/battlecruiser.svg?branch=dev)](https://travis-ci.org/13rentgen/battlecruiser)
   [![Coverage Status](https://coveralls.io/repos/github/13rentgen/battlecruiser/badge.svg?branch=dev)](https://coveralls.io/github/13rentgen/battlecruiser?branch=dev)
 
---
+++ test/BaseClasses/BaseClass.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var
+    PropChecker = require('../../lib/PropChecker'),
+    BaseClass   = require('../../lib/BaseClasses/BaseClass'),
+
+    staticProps = [];
+
+describe('Testing "lib/BaseClasses/Battlecruiser"...', function() {
+    describe('validate options', function() {
+        it('should not throw error when options correct', function() {
+            var
+                TestClass = class TestClass extends BaseClass {
+                },
+                testCases = [
+                    {},
+                    {
+                        foo: 'bar'
+                    },
+                    {
+                        age: 31
+                    }
+                ];
+
+            TestClass.optionsValidation = {
+                foo: PropChecker.isString,
+                age: PropChecker.isNumber
+            };
+
+            testCases.forEach(testCase => {
+                expect(() => new TestClass(testCase)).to.not.throw();
+            });
+        });
+
+        it('should throw error when options incorrect', function() {
+            var
+                TestClass = class TestClass extends BaseClass {
+                },
+                testCases = [
+                    {},
+                    {
+                        foo: 28
+                    },
+                    {
+                        foo: 'bar',
+                        age: 'bar'
+                    }
+                ];
+
+            TestClass.optionsValidation = {
+                foo: [PropChecker.isRequired, PropChecker.isString],
+                age: PropChecker.isNumber
+            };
+
+            testCases.forEach(testCase => {
+                expect(() => new TestClass(testCase)).to.throw(Error);
+            });
+        });
+    });
+});
