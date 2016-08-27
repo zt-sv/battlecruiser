61bdb64c4411c6f1452ebf270fdb0ec6836edf23
13rentgen <13rentgen@gmail.com>
2016-08-27 03:16:02 +0300

    Models factory

--- test/BaseClasses/Model.js
+++ test/BaseClasses/Model.js
@@ -4,60 +4,110 @@ var
     Model   = require('../../lib/BaseClasses/Model');
 
 describe('Testing "lib/BaseClasses/Model"...', function() {
-    describe('Create model', function() {
-        it('should create model', function() {
-            var
-                Foo = new Model({
-                    foo: Model.isString,
-                    bar: Model.isNumber
-                });
+    it('should create empty model', function() {
+        var
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: Model.isNumber
+            }),
 
-            expect(Foo).to.have.ownProperty('foo');
-            expect(Foo).to.have.ownProperty('bar');
-        });
+            fooInstance = new Foo();
 
-        it('should have validation', function() {
-            var
-                Foo = new Model({
-                    foo: Model.isString,
-                    bar: Model.isNumber
-                });
+        expect(fooInstance).to.have.ownProperty('foo');
+        expect(fooInstance).to.have.ownProperty('bar');
+    });
 
-            expect(() => Foo.foo = 'asdf').to.not.throw();
-            expect(() => Foo.bar = 123).to.not.throw();
-            expect(() => Foo.foo = 123).to.throw(Error, 'Property "foo"');
-            expect(() => Foo.bar = 'asd').to.throw(Error, 'Property "bar"');
-        });
+    it('should create model with props', function() {
+        var
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: Model.isNumber
+            }),
 
-        it('should have isolation properties', function() {
-            var
-                Foo = new Model({
-                    foo: Model.isString,
-                    bar: Model.isNumber
-                }),
+            fooInstance = new Foo({
+                foo: '123',
+                bar: 123
+            });
 
-                Bar = new Model({
-                    foo: Model.isString,
-                    bar: Model.isNumber
-                });
+        expect(fooInstance).to.have.ownProperty('foo');
+        expect(fooInstance).to.have.ownProperty('bar');
+    });
 
-            Foo.foo = 'first';
-            Bar.foo = 'second';
+    it('should throw error when property have no type', function() {
+        var
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: ''
+            });
 
-            expect(Foo.foo).to.not.equal(Bar.foo);
-        });
+        expect(() => new Foo()).to.throw(Error, 'should have a type');
+    });
 
-        it('default properties', function() {
-            var
-                DEFAULT_VALUE = 'asd',
-                Foo = new Model({
-                    foo: Model.isString,
-                    bar: Model.isNumber
-                }, {
-                    foo: DEFAULT_VALUE
-                });
+    it('should have validation', function() {
+        var
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: Model.isNumber
+            }),
 
-            expect(DEFAULT_VALUE).to.be.equal(Foo.foo);
-        });
+            fooInstance = new Foo();
+
+        expect(() => fooInstance.foo = 'asdf').to.not.throw();
+        expect(() => fooInstance.bar = 123).to.not.throw();
+        expect(() => fooInstance.foo = 123).to.throw(Error, 'Property "foo"');
+        expect(() => fooInstance.bar = 'asd').to.throw(Error, 'Property "bar"');
+
+        expect(() => new Foo({
+            foo: '123'
+        })).to.not.throw();
+        expect(() => new Foo({
+            foo: 123
+        })).to.throw(Error, 'Property "foo"');
+    });
+
+
+    it('should have isolation properties', function() {
+        var
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: Model.isNumber
+            }),
+
+            fooInstance = new Foo(),
+            barInstance = new Foo();
+
+        fooInstance.foo = 'first';
+        barInstance.foo = 'second';
+
+        expect(fooInstance.foo).to.not.equal(barInstance.foo);
+    });
+
+    it('default properties', function() {
+        var
+            DEFAULT_VALUE = 'asd',
+
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: Model.isNumber
+            }, {
+                foo: DEFAULT_VALUE
+            }),
+
+            fooInstance = new Foo();
+
+        expect(DEFAULT_VALUE).to.be.equal(fooInstance.foo);
+    });
+
+    it('should to extend Model', function() {
+        var
+            Foo = Model.create('Foo', {
+                foo: Model.isString,
+                bar: Model.isNumber
+            }),
+
+            fooInstance = new Foo();
+
+        expect(fooInstance).to.be.instanceof(Model);
+        expect(fooInstance).to.be.instanceof(Foo);
     });
 });
